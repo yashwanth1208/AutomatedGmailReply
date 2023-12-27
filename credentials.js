6f943e7a748ad323fae3b886b39114126f97c2bb
yash <yash@gmail.com>
2023-12-27 15:34:00 +0530

    changes

--- credentials.js
+++ credentials.js
@@ -1,6 +1,5 @@
-
-const CLIENT_ID = "Your Client Id";
-const CLEINT_SECRET = "Your Client Secret";
+const CLIENT_ID = "805384216996-dncm88n3jsfskdguilglen6ar0l778vf.apps.googleusercontent.com";
+const CLEINT_SECRET = "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";
 const REDIRECT_URI = "https://developers.google.com/oauthplayground";
-const REFRESH_TOKEN ="Your Refresh Token";
+const REFRESH_TOKEN ="1//04EVuiXDiLaw8CgYIARAAGAQSNwF-L9Ir3cLymbji-CmJRIC8E8qD8S3wm0jzCCPs_qMwqNFOA4RK3mzjPGotCHQZfiKe5SFGiSg";
 module.exports = { CLIENT_ID, CLEINT_SECRET, REDIRECT_URI, REFRESH_TOKEN };
